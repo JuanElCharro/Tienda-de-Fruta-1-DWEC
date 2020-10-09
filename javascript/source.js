a17d6e9a3e8e26c6cf717b362ff2e3717245dcf3
Juan García Hernández <juan@juan>
2020-10-09 19:22:27 +0200

    Retoques y Pruebas Javascript

--- javascript/source.js
+++ javascript/source.js
@@ -1,3 +1,65 @@
-function uno(){
-    
+function addPiña(){
+    let fruta = 4;
+    alert("Añadido un kilo de Piña");
+    return fruta;
+}
+
+function addPomelo(){
+    let fruta = 15;
+    alert("Añadido un kilo de Pomelo");
+    return fruta;
+}
+
+function addCastaña(){
+    let fruta = 1;
+    alert("Añadido un kilo de Castaña ");
+    return fruta;
+}
+
+function addCoco(){
+    let fruta = 6;
+    alert("Añadido un kilo de Coco ");
+    return fruta;
+}
+
+function addFresa(){
+    let fruta = 2;
+    alert("Añadido un kilo de Fresa ");
+    return fruta;
+}
+
+function addGrosella(){
+    let fruta = 10;
+    alert("Añadido un kilo de Grosella ");
+    return fruta;
+}
+
+function addHigo(){
+    let fruta = 5;
+    alert("Añadido un kilo de Higo ");
+    return fruta;
+}
+
+function addLichi(){
+    let fruta = 20;
+    alert("Añadido un kilo de Lichi ");
+    return fruta;
+}
+
+function addMora(){
+    let fruta = 7;
+    alert("Añadido un kilo de Mora ");
+    return fruta;
+}
+
+function addNaranja(){
+    let fruta = 2;
+    alert("Añadido un kilo de Naranja ");
+    return fruta;
+}
+
+function addPapaya(){
+    let fruta = 8;
+    alert("Añadido un kilo de Papaya ");
+    return fruta;
 }
